38aaaeb18c7519d7bd9dc395d2ff31233f043762
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-06-11 14:23:23 -0400

    Add MyComponents project and update solution configuration

--- Solution/MyComponents/Startup.cs
+++ Solution/MyComponents/Startup.cs
@@ -8,7 +8,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddDashboardComponents(this IServiceCollection services)
     {
         services.AddScoped<IDashboardComponent, SalesChart>();
-        services.AddScoped<IDashboardComponent, KPISummary>();
+        // services.AddScoped<IDashboardComponent, KPISummary>();
         services.AddScoped<IDashboardComponent, TeamActivity>();
         
         return services;
